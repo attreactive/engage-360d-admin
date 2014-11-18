c166d9fec4a827aadd213e2107097fd56a5e7b34
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2014-11-18 19:08:30 +0300

    many-to-one select

---
+++ components/form/ManyToOneSelect.jsx
@@ -0,0 +1,37 @@
+/**
+ * Engage-360D Admin
+ * @jsx React.DOM
+ */
+
+var React = require("react");
+var Select = require('./Select');
+
+var ManyToOneSelect = React.createClass({
+  render: function() {
+    var dependency = this.props.dependency;
+
+    var options = this.props.dependencyItems.map(function(item) {
+      return {
+        value: item[dependency.getIdProperty()],
+        text: dependency.stringify(item)
+      }
+    });
+
+    var link = {
+      value: this.props.valueLink.value,
+      requestChange: function(value) {
+        this.props.valueLink.requestChange(value);
+        this.props.setErrorMessageInputBlured();
+      }.bind(this)
+    };
+
+    return (
+      <Select type="value"
+              style={{width: 452}}
+              options={options}
+              valueLink={link} />
+    );
+  }
+});
+
+module.exports = ManyToOneSelect;
